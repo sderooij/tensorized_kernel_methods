4a0d7bb855100caa3f4487c87a7227426f6b42bb
Jetze Schuurmans <jetzeschuurmans@gmail.com>
2022-10-04 15:27:06 +0200

    remove batched tkm object

--- src/tkm/model.py
+++ src/tkm/model.py
@@ -6,9 +6,10 @@ from jax import random
 # from jax.scipy.linalg import kharti_rao
 
 from tkm.features import polynomial, compile_feature_map, fourier
-from tkm.utils import dotkron, vmap_dotkron, batched_dotkron
+from tkm.utils import dotkron, vmap_dotkron, batched_dotkron, vmap_dotkron_new
 from jax import jit,vmap
 # import jmp
+
 
 def dotkron(batch_size=None):
     if batch_size is None:
@@ -16,10 +17,11 @@ def dotkron(batch_size=None):
     else:
         return partial(batched_dotkron, batch_size=batch_size)
 
+
 class TensorizedKernelMachine(object):
     def __init__(
         self, 
-        dotkron = dotkron(batch_size=None)
+        dotkron = dotkron(batch_size=None),
         policy = None,
         **kwargs,
     ):
@@ -32,8 +34,6 @@ class TensorizedKernelMachine(object):
 
         self.fit = jit(partial(self.fit, **kwargs))
         self.predict = jit(partial(self.predict_vmap, **kwargs))
-
-
 
     def fit(
         self,
@@ -125,8 +125,6 @@ class TensorizedKernelMachine(object):
         # TODO: my_policy.cast_to_output(y)
         return W #, loss #error 
 
-
-
     def predict(
         self,
         X, 
@@ -174,94 +172,3 @@ def loss_function(C,x,y,regularization):
 @jit
 def error(C,x,y):
     return jnp.mean(jnp.sign(C.dot(x)) != y)
-
-
-class BatchedTKM(TensorizedKernelMachine):
-    # def __init__(self, batch_size, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
-
-    def fit(
-        self,
-        key,
-        X,
-        y,
-        M: int = 8,
-        R: int = 10,
-        l: float = 1e-5,
-        lengthscale: float = 0.5,
-        numberSweeps: int = 10,
-        feature_map=polynomial,
-        W = None,
-        **kwargs,
-    ):
-        """
-        input
-            X: data
-            y: labels
-            M: M_hat degree of polynomials in one of the CP legs 
-            R: rank
-            l: peanalty term of regularisation (denoted by lambda)
-            lenghtscale: hyperparameters of features
-            numberSweeps: number of ALS sweeps, 1 -> D -> 1 (not including last one)
-                1 and D are covered once
-                middle is covered twice
-                alternative is doing linear pas 1,...,D (not in this code)
-
-        returns
-            weights
-            loss
-            error: list of errors per ALS step
-        """
-        # TODO: jmp policy
-        # my_policy = self.policy
-        # params, x = my_policy.cast_to_compute((params, x))
-
-        # feature_map = feature_list[feature_idx]
-
-        features = compile_feature_map(feature_map, M=M, lengthscale=lengthscale)
-        # polynomial_compiled = jit(partial(polynomial, M=M))
-
-        N,D = X.shape #jnp.shape(X)
-        W = random.normal(key, shape=(D,M,R)) if W is None else W
-        # list(range(D)) # TODO: JAX
-        Matd = jnp.ones((N,R))
-        reg = jnp.ones((R,R))
-        
-        # initializaiton of cores
-        # intializing with the constant cores already contracted
-        for d in range(D-1, -1, -1):
-            W = W.at[d].divide(jnp.linalg.norm(W[d])) if W is None else W       # TODO: check if this is necessary
-            reg *= jnp.dot(W[d].T, W[d])           # reg has shape R * R
-            Mati = features(X[:,d])
-            Matd *= jnp.dot(Mati, W[d])            # Matd has shape N * R, contraction of phi_x_d * w_d
-
-        # D,M,R = W.shape
-        # itemax = numberSweeps * D # numberSweeps *(2*(D-1))+1;    # not necesarry in python
-        # loss = []
-        # error = []
-        # i=0
-        for s in range(numberSweeps): #TODO fori jax loop
-            for d in range(D): #TODO fori jax loop
-                # compute phi(x_d)
-                Mati = features(X[:,d])                               
-                # undoing the d-th element from Matd (contraction of all cores)
-                Matd /= jnp.dot(Mati, W[d])                                      
-                # C = vmap_dotkron(Mati,Matd)                                  # N by M_hat*R
-                CC, Cy = batched_dotkron(Mati, Matd, y, **kwargs)
-                reg /= jnp.dot(W[d].T, W[d])                                    # regularization term
-                regularization = l * jnp.kron(reg, jnp.eye(M)) # TODO: this results in sparse matrix, check if multiplications with 0 need to be avoided
-                x = jnp.linalg.solve(                                   # solve systems of equations
-                    (CC + regularization), 
-                    Cy
-                )
-                # loss.append(float(loss_function(C,x,y,regularization)[0][0]))
-                # print(error(C,x,y))
-                # loss = jnp.linalg.norm(C @ x - y)**2 + x.T @ regularization @ x )  #TODO check if **2 is necessary (can it be done in function call of norm)
-                # error =  jnp.mean(jnp.sign(C @ x) != y) # TODO not equal elementwise   # classification; for regression mean(((C*x)-y).^2)
-                W = W.at[d].set( x.reshape((M,R)) )
-                reg *= jnp.dot(W[d].T, W[d])
-                Matd *= jnp.dot(Mati, W[d])
-                # i+=1
-        
-        # TODO: my_policy.cast_to_output(y)
-        return W #, loss #error 
