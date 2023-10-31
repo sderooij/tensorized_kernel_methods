ff3f4681d906f87e2b4317d34a7f8483bea19eb2
Seline de Rooij <67693178+sderooij@users.noreply.github.com>
2023-10-31 15:29:14 +0100

    change the model function and initialization to match sklearns interface

--- src/tkm/features.py
+++ src/tkm/features.py
@@ -46,11 +46,11 @@ def fourier(
     **kwargs,
 ): 
     """
-    function Mati = features(X,M,lengthscale)   # fourier features, but can be any polynomials is easiest
-    X = (X+1/2)/2;
+    function Mati = features(x,M,lengthscale)   # fourier features, but can be any polynomials is easiest
+    x = (x+1/2)/2;
     w = 1:M;
     S = sqrt(2*pi) * lengthscale*exp(-(pi*w/2).^2*lengthscale^2/2);
-    Mati = sinpi(X*w).*sqrt(S);
+    Mati = sinpi(x*w).*sqrt(S);
     end
     """
     X = (X+1/2)/2
--- src/tkm/model.py
+++ src/tkm/model.py
@@ -15,10 +15,14 @@ from jax import jit,vmap
 class TensorizedKernelMachine(object):
     def __init__(
         self, 
-        features = polynomial,
+        features=polynomial,
         M: int = 8,
         R: int = 10,
-        lengthscale: float = 0.5,  
+        lengthscale: float = 0.5,
+        l: float = 1e-5,
+        numberSweeps: int = 10,
+        key=random.PRNGKey(0),
+        W=None,
         dotkron = get_dotkron(batch_size=None),
         **kwargs,
     ):
@@ -30,87 +34,137 @@ class TensorizedKernelMachine(object):
             lenghtscale: hyperparameters of fourierfeatures
             get_dotkron: function used for rowwise kronnecker product
             batch_size: if is not None batched_dotkron will be used
-        """
-
-        self.dotkron = jit(dotkron)
-        self.features = jit(partial(features, M=M, R=R, lengthscale=lengthscale, **kwargs))
-        self.fit = jit(partial(self.fit, M=M, R=R, **kwargs))
-        self.predict = jit(partial(self.predict, **kwargs))
-
-    def fit(
-        self,
-        key,
-        X,
-        y,
-        l: float = 1e-5,
-        numberSweeps: int = 10,
-        M: int = 8,
-        R: int = 10,
-        W = None,
-        **kwargs,
-    ):
-        """
-        Args:
-            X: data
-            y: labels
             l: peanalty term of regularisation (denoted by lambda)
             numberSweeps: number of ALS sweeps, 1 -> D -> 1 (not including last one)
                 1 and D are covered once
                 middle is covered twice
                 alternative is doing linear pas 1,...,D (not in this code)
-            M: M_hat degree of polynomials in one of the CP legs 
-            R: rank
             W: device array of weight, used to bypass random init
+        """
+        self.M = M
+        self.R = R
+        self.l = l
+        self.numberSweeps = numberSweeps
+        self.lengthscale = lengthscale
+        self.W = W
+        self.key = key
+        self.dotkron = jit(dotkron)
+        self.features = jit(partial(features, M=M, R=R, lengthscale=lengthscale, **kwargs))
+        self.fit = jit(partial(self.fit, **kwargs))
+        self.predict = jit(partial(self.predict, **kwargs))
+
+    def fit(
+        self,
+        x,
+        y,
+        **kwargs,
+    ):
+        """
+        Args:
+            x: data
+            y: labels
 
         Returns:
             weights: device array
         """
 
-        N,D = X.shape
-        if W is None:
-            W = random.normal(key, shape=(D,M,R)) if W is None else W
-            W /= jnp.linalg.norm(W, axis=(1,2), keepdims=True)
+        N,D = x.shape
+        if self.W is None:
+            self.W = random.normal(self.key, shape=(D, self.M, self.R))
+            self.W /= jnp.linalg.norm(self.W, axis=(1, 2), keepdims=True)
         
-        reg = jnp.ones((R,R))
-        self.init_reg = jit(partial(self.init_reg, W=W)) #TODO: check the memory footprint of partially filling W
+        reg = jnp.ones((self.R, self.R))
+        self._init_reg = jit(partial(self._init_reg, W=self.W)) #TODO: check the memory footprint of partially filling W
         reg = fori_loop(0, D, self.init_reg, init_val=reg)
         
-        self.init_matd = jit(partial(self.init_matd, W=W, X=X)) #TODO: check the memory footprint of partially filling W and X
-        Matd = jnp.ones((N,R))
+        self.init_matd = jit(partial(self._init_matd, W=self.W, X=x)) #TODO: check the memory footprint of partially
+        # filling W and x
+        Matd = jnp.ones((N, self.R))
         Matd = fori_loop(0, D, self.init_matd, init_val=Matd)
 
-        self.fit_step = jit(partial(self.fit_step, X=X, y=y, l=l, M=M, R=R))
-        self.sweep = jit(partial(self.sweep, D=D))
+        self.fit_step = jit(partial(self._fit_step, X=x, y=y))
+        self.sweep = jit(partial(self._sweep, D=D))
             
-        (Matd, W, reg) = fori_loop(
-            0, numberSweeps, self.sweep, init_val=(Matd, W, reg)
+        (Matd, self.W, reg) = fori_loop(
+            0, self.numberSweeps, self.sweep, init_val=(Matd, self.W, reg)
         )
         
-        return W
+        return self
     
-    def init_reg(self, d, reg, W): # TODO: forloop is not necessary, should be able to do this with linalg
+    def _init_reg(self, d, reg, W): # TODO: forloop is not necessary, should be able to do this with linalg
+        """
+        Computes the regularization term for the d-th factor matrix of the weight tensor.
+
+        Args:
+            d (int): The factor matrix of the weight tensor to compute the regularization term for.
+            reg (float): The regularization strength.
+            W (jax.interpreters.xla.DeviceArray): The weight tensor.
+
+        Returns:
+            jax.interpreters.xla.DeviceArray: The regularization term for the d-th factor matrix of the weight tensor.
+        """
         reg *= jnp.dot(W[d].T, W[d])           # reg has shape R * R    
         return reg
     
-    def init_matd(self, d, Matd, X, W):
+    def _init_matd(self, d, Matd, X, W):
+        """
+        Initializes the Matd matrix for a given dimension d.
+
+        Args:
+            d (int): The dimension for which to initialize Matd.
+            Matd (jax.numpy.ndarray): The Matd matrix to initialize.
+            X (jax.numpy.ndarray): The input data matrix of shape (N, D).
+            W (jax.numpy.ndarray): The weight matrix of shape (D, R).
+
+        Returns:
+            jax.numpy.ndarray: The initialized Matd matrix of shape (N, R).
+        """
         Mati = self.features(X[:,d])
         Matd *= jnp.dot(Mati, W[d])            # Matd has shape N * R, contraction of phi_x_d * w_d
         return Matd
     
-    def sweep(self, i, value, D):
+    def _sweep(self, i, value, D):
+        """
+        Perform a sweep over the i-th factor matrix of a CP tensor of shape (I_1, ..., I_i, ..., I_D),
+        apply the `fit_step` function to each factor matrix, using `value` as the initial value.
+
+        Args:
+            i (int): The index of the dimension to sweep over.
+            value (jax.numpy.ndarray): The initial value to use for factor matrix.
+            D (int): The size of the dimension to sweep over.
+
+        Returns:
+            jax.numpy.ndarray: The final value obtained after applying `fit_step` for each factor matrix.
+        """
         return fori_loop(0, D, self.fit_step, init_val=value)
     
-    def fit_step(self, d, value, l, M, R, X, y):
+    def _fit_step(self, d, value, X, y):
+        """
+        Perform a single step of the ALS algorithm to fit the model to the data.
+
+        Args:
+            d (int): The index of the current weight factor matrix.
+            value (Tuple[jnp.ndarray, jnp.ndarray, float]): A tuple containing the current feature tensor Matd,
+                the weight tensor W, and the regularization parameter reg.
+            X (jnp.ndarray): The input data matrix of shape (n_samples, n_features).
+            y (jnp.ndarray): The target values of shape (n_samples,).
+
+        Returns:
+            Tuple[jnp.ndarray, jnp.ndarray, float]: A tuple containing the updated feature tensor Matd,
+                the updated weight tensor W, and the updated regularization parameter reg.
+        """
+    def _fit_step(self, d, value, X, y):
         (Matd, W, reg) = value # TODO: jit(partial()) this
         Mati = self.features(X[:,d])     # compute phi(x_d)                          
         Matd /= jnp.dot(Mati, W[d])     # undoing the d-th element from Matd (contraction of all cores)
         CC, Cy = self.dotkron(Mati,Matd,y)                                  # N by M_hat*R
         
         reg /= jnp.dot(W[d].T, W[d])                                    # regularization term
-        regularization = l * jnp.kron(reg, jnp.eye(M)) # TODO: this results in sparse matrix, check if multiplications with 0 need to be avoided                
+        regularization = self.l * jnp.kron(reg, jnp.eye(self.M)) # TODO: this results in sparse matrix,
+        # check if multiplications with 0 need to be avoided
         
-        x = jnp.linalg.solve((CC + regularization), Cy)         # solve systems of equation, least squares
-        W = W.at[d].set( x.reshape((M,R)) )
+        w_d = jnp.linalg.solve((CC + regularization), Cy)         # solve systems of equation, least squares
+        W = W.at[d].set(w_d.reshape((self.M, self.R)) )
         reg *= jnp.dot(W[d].T, W[d])
         Matd *= jnp.dot(Mati, W[d])
 
@@ -118,9 +172,9 @@ class TensorizedKernelMachine(object):
 
     def predict(
         self,
-        X, 
-        W,
-        *args,**kwargs,
+        X,
+        *args,
+        **kwargs,
     ):
         """
         Args:
@@ -133,4 +187,4 @@ class TensorizedKernelMachine(object):
         """
         return vmap(
             lambda x,y :jnp.dot(self.features(x),y), (1,0),
-        )(X, W).prod(0).sum(1)
+        )(X, self.W).prod(0).sum(1)
